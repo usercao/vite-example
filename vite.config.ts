018fb0c140d62ff82f27116771359c4806e79fda
usercao <cao858646882@qq.com>
2023-07-21 11:08:14 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
+import { createLogger, defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc'
+import UnoCSS from 'unocss/vite';
+import { createMpaPlugin } from 'vite-plugin-virtual-mpa';
+import { fileURLToPath, URL } from 'node:url';
+import type { Logger } from 'vite';
+
+const customLogger = (): Logger => {
+  const logger = createLogger();
+  return {
+    ...logger,
+    warn: (message, options) => {
+      const regexp = new RegExp('files in the public directory are served at the root path.', 'g');
+      if (regexp.test(message)) return;
+      logger.info(message, options);
+    },
+  };
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  customLogger: customLogger(),
+  plugins: [react(),
+    UnoCSS(),
+    createMpaPlugin({
+      verbose: false,
+      template: 'public/index.html',
+      pages: [{ name: 'index', filename: 'index.html' }],
+      rewrites: [{ from: /^(?!.*charting_library).*$/i, to: '/public/index.html' }],
+    }),],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
 })
