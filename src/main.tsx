cb86d70dba676734800ee3d73b11665cfb1954b1
usercao <cao858646882@qq.com>
2023-07-21 15:39:06 +0800

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'virtual:uno.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode>{/* <App /> */}</React.StrictMode>);
+// ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode>{/* <App /> */}</React.StrictMode>);
+
+function run() {
+  const container = window.document.getElementById('root')!;
+  const root = ReactDOM.createRoot(container);
+  root.render(<React.StrictMode>{/* <App /> */}</React.StrictMode>);
+}
+
+run();
